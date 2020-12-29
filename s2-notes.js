2f895f34566ff1520d85d5e68542ac8522e23b49
dwadden <dwadden@cs.washington.edu>
2020-12-29 15:33:24 -0800

    Frontend code works.

--- s2-notes.js
+++ s2-notes.js
@@ -8,33 +8,47 @@
 // ==/UserScript==
 
 
+function import_simplemde() {
+    // Import the SimpleMDE library: https://simplemde.com/.
 
-function addme() {
-    console.log(document.readyState);
+    // Get the javascript. See https://stackoverflow.com/questions/19737031/loading-scripts-after-page-load.
+    const tag = document.createElement("script");
+    tag.src = "https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"
+    document.getElementsByTagName("head")[0].appendChild(tag);
 
-    // Uses template literals
+    // Get the CSS. See https://stackoverflow.com/questions/30752482/fetch-css-file-after-page-loads.
+    const styles = "@import url('https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css');";
+    const newSS=document.createElement('link');
+    newSS.rel='stylesheet';
+    newSS.href='data:text/css,'+escape(styles);
+    document.getElementsByTagName("head")[0].appendChild(newSS);
+}
+
+function add_notes_field() {
+    // Modify the HTML page source to add notes.
+
+    // Create a literal for the HTML to insert.
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
     const my_html = `<div id="notes" class="card">
-    <div class="card-header">
-      <div class="card-header-title">
-        <h4 class="card-header-title__title">Notes.</h4>
-      </div>
-    </div>
-    <div class="card-content">
-      <div class="card-content-main">
-        <textarea id="note-text" name="note-text" style="box-sizing:border-box; width:100%; height:100%;">
-        </textarea>
-      </div>
-    </div>
-  </div>`;
+        <div class="card-content">
+          <div class="card-content-main">
+            <textarea id="note-text" name="note-text"></textarea>
+          </div>
+        </div>
+      </div>`;
 
     // Add the HTML.
     const currentDiv = document.getElementById("extracted");
     currentDiv.insertAdjacentHTML("beforebegin", my_html)
-}
 
+    // Convert to markdown.
+    // Uses https://github.com/sparksuite/simplemde-markdown-editor.
+    const simplemde = new SimpleMDE({ element: document.getElementById("note-text") });
+}
 
 (function() {
     'use strict';
-    window.addEventListener("load", addme)
+    window.addEventListener("load", import_simplemde);
+    // Wait a bit to make sure the libraries get loaded.
+    window.addEventListener("load", () => setTimeout(add_notes_field, 100));
 })();

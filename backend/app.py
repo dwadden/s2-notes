dc4b5a287e514fc5de3e8ca721774c631730f9da
dwadden <dwadden@cs.washington.edu>
2020-12-29 18:39:31 -0800

    If the file's missing, just don't return anything.

--- backend/app.py
+++ backend/app.py
@@ -11,35 +11,40 @@ CORS(app)
 NOTES_DIR = "/Users/dwadden/Google-Drive/uw/research/paper-notes"
 
 
-@app.route('/GetNotes/<int:paper_id>')
+@app.route("/GetNotes/<int:paper_id>")
 def get_notes(paper_id):
-    if not os.path.exists(f'{NOTES_DIR}/{paper_id}.json'):
-        return 'File does not exist', 404
+    if not os.path.exists(f"{NOTES_DIR}/{paper_id}.json"):
+        # If notes don't exist for this paper, just return an empty string.
+        data = {"paper_id": None,
+                "title": None,
+                "notes": ""}
+    else:
+        # Otherwise load the data.
+        with open(f"{NOTES_DIR}/{paper_id}.json") as f:
+            data = json.load(f)
 
-    with open(f'{NOTES_DIR}/{paper_id}.json') as f:
-        data = json.load(f)
-    return json.dumps(data)
+    return jsonify(data)
 
 
-@app.route('/SetNotes', methods=['POST'])
+@app.route("/SetNotes", methods=["POST"])
 def set_notes():
     if not request.json:
-        return 'No data passed in', 400
-    elif 'paper_id' not in request.json:
-        return 'Missing paper_id field', 400
-    elif 'title' not in request.json:
-        return 'Missing title field', 400
+        return "No data passed in", 400
+    elif "paper_id" not in request.json:
+        return "Missing paper_id field", 400
+    elif "title" not in request.json:
+        return "Missing title field", 400
 
-    paper_id = request.json['paper_id']
+    paper_id = request.json["paper_id"]
     file_json = {
         "paper_id": paper_id,
-        'title': request.json['title'],
-        'notes': request.json.get('notes', ""),
+        "title": request.json["title"],
+        "notes": request.json.get("notes", "")
     }
 
-    with open(f'{NOTES_DIR}/{paper_id}.json', 'w') as f:
+    with open(f"{NOTES_DIR}/{paper_id}.json", "w") as f:
         json.dump(file_json, f)
 
     return jsonify({
-        'data saved to ' + str(paper_id) + '.json': file_json
+        "data saved to " + str(paper_id) + ".json": file_json
         }), 201
